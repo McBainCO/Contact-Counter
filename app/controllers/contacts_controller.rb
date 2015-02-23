389960ae50769c4865c06d3bd6770d4672198e30
McBainCO <grahammcbain@gmail.com>
2015-02-23 16:46:11 -0700

    creds changed and colors fixed

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -4,12 +4,14 @@ class ContactsController < ApplicationController
   end
 
   def all
+  	@color = ["#2C3E50", "#FC4349", "#6DBCDB", "#F7E248", "#D7DADB", "#FFF"]
+
   	data = $cio_connect.list_contacts(:account => $cio_acct)
 
   	@contacts = JSON.parse(data.body)["matches"]
   	
   	@converted = @contacts.map do |contact|
-  					{title: contact["name"] , value: contact["sent_count"] ,  color: "#2C3E50" }
+  					{title: contact["name"] , value: contact["received_count"] ,  color: @color.sample }
   				end
 
   	render json: @converted
